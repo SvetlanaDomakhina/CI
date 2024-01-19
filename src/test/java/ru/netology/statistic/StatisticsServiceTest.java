52378ebf7f77cf3ae198ba447b7fd76aff1816ae
Svetlana Domakhina <domakhina.s@gmail.com>
2024-01-19 14:32:01 -0800

    CI2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,65 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 300, 8, 6, 11, 11, 12};
+        long expected = 300;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfItsTheLastOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 44};
+        long expected = 44;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfItsTheFirstOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {69, 5, 8, 4, 5, 30, 8, 6, 11, 11, 44};
+        long expected = 69;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 31, 8, 6, 11, 11, -122};
+        long expected = 31;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithZero() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 110, 11, -122};
+        long expected = 110;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
